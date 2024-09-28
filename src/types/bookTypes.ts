461836005ba93ea613ef1027d1c05ee2b6332f3c
mungase-narayan <narayanmungase03@gmail.com>
2024-09-28 12:19:09 +0530

    update Book interface

--- src/types/bookTypes.ts
+++ src/types/bookTypes.ts
@@ -4,6 +4,7 @@ import { User } from "./userTypes"
 export interface Book {
     _id: string;
     title: string;
+    description: string;
     author: User;
     genre: string;
     coverImage: string;
